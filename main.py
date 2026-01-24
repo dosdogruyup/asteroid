ed8ce7f9700fd4035b41297101cf431cd2716a21
dosdogruyup <berk-dmrdzn@hotmail.com>
2026-01-24 23:36:11 +0300

    constants.py added: have resolution

--- main.py
+++ main.py
@@ -1,8 +1,9 @@
 import pygame
+import constants
 
 def main():
     print(f"Starting Asteroids with pygame version: {pygame.version.ver}")
-
+    print(f"Screen width: {constants.SCREEN_WIDTH}\nScreen height: {constants.SCREEN_HEIGHT}")
 
 if __name__ == "__main__":
     main()
