d826b97790faa6e9a968143bd7b21c598cb69106
dosdogruyup <berk-dmrdzn@hotmail.com>
2026-01-25 04:50:15 +0300

    fixed up fps to 60

--- main.py
+++ main.py
@@ -7,7 +7,10 @@ def main():
     print(f"Screen width: {constants.SCREEN_WIDTH}\nScreen height: {constants.SCREEN_HEIGHT}")
 
     pygame.init()
+    
     screen = pygame.display.set_mode((constants.SCREEN_WIDTH, constants.SCREEN_HEIGHT))
+    clock = pygame.time.Clock()
+    dt = 0
 
     while True:
         log_state()
@@ -18,6 +21,8 @@ def main():
         
         screen.fill("black")
         pygame.display.flip()
+        
+        dt = clock.tick(60) / 1000
 
 
 if __name__ == "__main__":
