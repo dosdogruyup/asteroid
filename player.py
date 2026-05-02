11f1fd05a388ba56ad6e0a91919035fc61b7ce43
dosdogruyup <berk-dmrdzn@hotmail.com>
2026-05-03 00:58:03 +0300

    Made the shots fire less frequent

--- player.py
+++ player.py
@@ -1,12 +1,13 @@
 import pygame
 from shot import Shot
 from circleshape import CircleShape 
-from constants import PLAYER_RADIUS, LINE_WIDTH, PLAYER_TURN_SPEED, PLAYER_SPEED, PLAYER_SHOOT_SPEED
+from constants import PLAYER_RADIUS, LINE_WIDTH, PLAYER_TURN_SPEED, PLAYER_SPEED, PLAYER_SHOOT_SPEED, PLAYER_SHOOT_COOLDOWN_SECONDS
 
 class Player(CircleShape):
     def __init__(self, x, y):
         super().__init__(x, y, PLAYER_RADIUS)
         self.rotation = 0
+        self.shot_cooldown = PLAYER_SHOOT_COOLDOWN_SECONDS
     
     def triangle(self):
         forward = pygame.Vector2(0, 1).rotate(self.rotation)
@@ -23,6 +24,8 @@ class Player(CircleShape):
         self.rotation += PLAYER_TURN_SPEED * dt
 
     def update(self, dt):
+        self.shot_cooldown -= dt
+        
         keys = pygame.key.get_pressed()
 
         if keys[pygame.K_a]:
@@ -33,8 +36,9 @@ class Player(CircleShape):
             self.move(dt)
         if keys[pygame.K_s]:
             self.move(-dt)
-        if keys[pygame.K_SPACE]:
+        if keys[pygame.K_SPACE] and self.shot_cooldown <= 0:
             self.shoot()
+            self.shot_cooldown = PLAYER_SHOOT_COOLDOWN_SECONDS
     
     def move(self, dt):
         unit_vector = pygame.Vector2(0, 1)
